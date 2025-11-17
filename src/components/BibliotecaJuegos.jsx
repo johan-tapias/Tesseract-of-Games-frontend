27ebfb2afa068a76ffaffd64c3cb3d2c9a5e0e74
Johan Tapias <228216134+johan-tapias@users.noreply.github.com>
2025-11-16 21:13:54 -0500

    se actualizo el componente de la biblioteca y se agregaron sus estilos

--- src/components/BibliotecaJuegos.jsx
+++ src/components/BibliotecaJuegos.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { obtenerJuegos, eliminarJuego } from "../services/juegosService";
 import { Link } from "react-router-dom";
+import "./../styles/componentes.css";
 
 export default function BibliotecaJuegos() {
   const [juegos, setJuegos] = useState([]);
@@ -19,26 +20,39 @@ export default function BibliotecaJuegos() {
   };
 
   return (
-    <div>
-      <h2>Biblioteca de Juegos</h2>
+    <div className="biblioteca">
 
-      <Link to="/nuevo-juego">Agregar nuevo juego</Link>
+      <h1 className="titulo-pagina">Mi Biblioteca</h1>
 
-      {juegos.length === 0 && <p>No hay juegos aún.</p>}
+      {juegos.length === 0 && (
+        <p className="sin-juegos">No hay juegos aún. Agrega uno para empezar.</p>
+      )}
 
-      <ul>
+      <div className="grid-juegos">
         {juegos.map(juego => (
-          <li key={juego._id}>
-            {juego.titulo}
-            {" - "}
-            <Link to={`/juego/${juego._id}`}>Ver más</Link>
-            {" | "}
-            <Link to={`/editar/${juego._id}`}>Editar</Link>
-            {" | "}
-            <button onClick={() => borrar(juego._id)}>Eliminar</button>
-          </li>
+          <div key={juego._id} className="juego-card">
+
+            <div className="card-img-container">
+              <img
+                src={juego.imagenPortada || "https://via.placeholder.com/300"}
+                alt={juego.titulo}
+              />
+            </div>
+
+            <div className="card-info">
+              <h3>{juego.titulo}</h3>
+              <p className="card-detalle">{juego.genero} • {juego.plataforma}</p>
+
+              <div className="card-botones">
+                <Link to={`/juego/${juego._id}`} className="btn-card blue">Ver</Link>
+                <Link to={`/editar/${juego._id}`} className="btn-card yellow">Editar</Link>
+                <button onClick={() => borrar(juego._id)} className="btn-card red">Eliminar</button>
+              </div>
+            </div>
+
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
