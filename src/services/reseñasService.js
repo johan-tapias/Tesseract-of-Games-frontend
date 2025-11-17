a98d66e16a045fe6d888bca13d7b74a827238edd
Johan Tapias <228216134+johan-tapias@users.noreply.github.com>
2025-11-17 15:44:06 -0500

    se corrigieron errores

--- src/services/reseñasService.js
+++ src/services/reseñasService.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/reseñas";
+const API_URL = "http://localhost:5000/api/resenas";
 
 export const obtenerReseñas = () => axios.get(API_URL);
-export const obtenerReseñasPorJuego = (juegoId) => axios.get(`${API_URL}/juego/${juegoId}`);
-export const crearReseña = (data) => axios.post(API_URL, data);
-export const actualizarReseña = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const eliminarReseña = (id) => axios.delete(`${API_URL}/${id}`);
-export const obtenerReseña = (id) => axios.get(`${API_URL}/${id}`);
+
+export const obtenerReseñasPorJuego = (juegoId) =>
+  axios.get(`${API_URL}/juego/${juegoId}`);
+
+export const obtenerReseña = (id) =>
+  axios.get(`${API_URL}/${id}`);
+
+export const crearReseña = (data) =>
+  axios.post(API_URL, data);
+
+export const actualizarReseña = (id, data) =>
+  axios.put(`${API_URL}/${id}`, data);
+
+export const eliminarReseña = (id) =>
+  axios.delete(`${API_URL}/${id}`);
